f3b103054b594541562e34dacc1f5e0bac2d2a6a
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-21 15:14:26 +0700

    fix: correct web admin attendance controller namespace

--- routes/users/route-web-admin.php
+++ routes/users/route-web-admin.php
@@ -14,8 +14,8 @@ Route::group(['prefix' => 'web-admin', 'middleware' => ['checkUser:web'], 'as' =
     require __DIR__.'/../gradebook.php';
 
     // Kehadiran mahasiswa dapat dikelola Administrator tanpa berpindah guard Dosen.
-    Route::get('/akademik/kehadiran', [App\\Http\\Controllers\\Private\\Dosen\\AkademikOperasionalController::class, 'webAdminKehadiran'])->name('akademik.kehadiran');
-    Route::post('/akademik/kehadiran', [App\\Http\\Controllers\\Private\\Dosen\\AkademikOperasionalController::class, 'webAdminSimpanKehadiran'])->name('akademik.kehadiran.store');
+    Route::get('/akademik/kehadiran', [App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'webAdminKehadiran'])->name('akademik.kehadiran');
+    Route::post('/akademik/kehadiran', [App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'webAdminSimpanKehadiran'])->name('akademik.kehadiran.store');
 
     // MASTER AUTHORITY
     require __DIR__.'/../master-core.php';
