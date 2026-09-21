7ee7956eaca038e71cff7ebc7d259737994dd0b4
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-21 20:40:32 +0700

    Kunci lebar kolom Kelas KRS menjadi sangat sempit

--- resources/views/private/mahasiswa/akademik/cetak-krs.blade.php
+++ resources/views/private/mahasiswa/akademik/cetak-krs.blade.php
@@ -21,12 +21,12 @@ body { font-family: Arial, sans-serif; color:#111; font-size:12px; margin:0; }
 table { width:100%; border-collapse:collapse; }
 th,td { border:1px solid #222; padding:6px; }
 .krs-table { width:100%; table-layout:fixed; }
-.krs-table th:nth-child(1), .krs-table td:nth-child(1) { width:4%; }
-.krs-table th:nth-child(2), .krs-table td:nth-child(2) { width:12%; }
-.krs-table th:nth-child(3), .krs-table td:nth-child(3) { width:36%; }
-.krs-table th:nth-child(4), .krs-table td:nth-child(4) { width:6%; }
-.krs-table th:nth-child(5), .krs-table td:nth-child(5) { width:4%; padding-left:2px; padding-right:2px; text-align:center; vertical-align:middle; white-space:nowrap; overflow:hidden; }
-.krs-table th:nth-child(6), .krs-table td:nth-child(6) { width:38%; }
+.krs-table .c-no { width:4%; }
+.krs-table .c-kode { width:12%; }
+.krs-table .c-mk { width:39%; }
+.krs-table .c-sks { width:6%; }
+.krs-table .c-kelas { width:3%; text-align:center; white-space:nowrap; overflow:hidden; padding:4px 1px; }
+.krs-table .c-dosen { width:36%; }
 th { background:#eee; }
 .meta td { border:0; padding:3px 0; }
 .text-center{text-align:center}.text-right{text-align:right}
@@ -61,6 +61,54 @@ th { background:#eee; }
             <td style="width:105px"></td>
         </tr>
     </table>
+</div>
+<div style="text-align:center; margin-bottom:14px;">
+    <h3 style="margin:2px 0;">KARTU RENCANA STUDI (KRS)</h3>
+    <p style="margin:2px 0;">{{ $currentSemester->name ?? '' }} - {{ $currentSemester->type ?? '' }}</p>
+</div>
+<table class="meta">
+<tr><td width="18%">Nama</td><td>: {{ $mahasiswa->name ?? '-' }}</td><td width="18%">NIM</td><td>: {{ $mahasiswa->numb_nim ?? '-' }}</td></tr>
+<tr><td>Program Studi</td><td>: {{ $mahasiswa->programStudi->name ?? '-' }}</td><td>Semester</td><td>: {{ $mahasiswa->semester ?? '-' }}</td></tr>
+<tr><td>Status KRS</td><td>: {{ $krsHeader->status ?? 'Belum dibuat' }}</td><td>Kode KRS</td><td>: {{ $krsHeader->code ?? '-' }}</td></tr>
+</table>
+<br>
+<table>
+<thead><tr><th width="5%">No</th><th>Kode</th><th>Mata Kuliah</th><th>SKS</th><th>Kelas</th><th>Dosen</th></tr></thead>
+<tbody>
+@forelse($krs as $i => $item)
+<tr>
+<td class="text-center">{{ $i+1 }}</td>
+<td>{{ $item->mataKuliah->kode_mk ?? $item->mataKuliah->code ?? '-' }}</td>
+<td>{{ $item->mataKuliah->nama ?? $item->mataKuliah->name ?? '-' }}</td>
+<td class="text-center">{{ $item->sks ?? $item->mataKuliah->sks ?? 0 }}</td>
+<td>{{ $item->kelas->nama_kelas ?? $item->kelas->name ?? '-' }}</td>
+<td>{{ $item->dosen->nama_lengkap ?? $item->dosen->name ?? '-' }}</td>
+</tr>
+@empty
+<tr><td colspan="6" class="text-center">Belum ada mata kuliah dalam KRS.</td></tr>
+@endforelse
+</tbody>
+<tfoot><tr><th colspan="3" class="text-right">Total SKS</th><th class="text-center">{{ $krs->sum('sks') }}</th><th colspan="2"></th></tr></tfoot>
+</table>
+<div class="signature"><div class="signature-box"><p>Mengetahui,<br>Dosen Pembimbing Akademik</p><br><br><br><strong>{{ $krsHeader->dosenPA?->name ?? '________________________' }}</strong></div></div>
+</body>
+</html><table class="krs-table">
+<thead><tr><th class="c-no">No</th><th class="c-kode">Kode</th><th class="c-mk">Mata Kuliah</th><th class="c-sks">SKS</th><th class="c-kelas">Kelas</th><th class="c-dosen">Dosen</th></tr></thead>
+<tbody>
+@forelse($krs as $i => $item)
+<tr>
+<td class="text-center">{{ $i+1 }}</td>
+<td>{{ $item->mataKuliah->kode_mk ?? $item->mataKuliah->code ?? '-' }}</td>
+<td>{{ $item->mataKuliah->nama ?? $item->mataKuliah->name ?? '-' }}</td>
+<td class="text-center">{{ $item->sks ?? $item->mataKuliah->sks ?? 0 }}</td>
+<td class="c-kelas">{{ $item->kelas->nama_kelas ?? $item->kelas->name ?? '-' }}</td>
+<td>{{ $item->dosen->nama_lengkap ?? $item->dosen->name ?? '-' }}</td>
+</tr>
+@empty
+<tr><td colspan="6" class="text-center">Belum ada mata kuliah dalam KRS.</td></tr>
+@endforelse
+</tbody>
+<tfoot><tr><th colspan="3" class="text-right">Total SKS</th><th class="text-center">{{ $krs->sum('sks') }}</th><th colspan="2"></th></tr></table>
 </div>
 <div style="text-align:center; margin-bottom:14px;">
     <h3 style="margin:2px 0;">KARTU RENCANA STUDI (KRS)</h3>
