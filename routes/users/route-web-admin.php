62d38e60e80008a30394a4c98ad1a157c7729afb
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-21 15:14:17 +0700

    feat: add web admin attendance routes

--- routes/users/route-web-admin.php
+++ routes/users/route-web-admin.php
@@ -13,6 +13,10 @@ Route::group(['prefix' => 'web-admin', 'middleware' => ['checkUser:web'], 'as' =
     // GRADEBOOK: controlled grade entry and workflow (loaded before legacy nilai routes)
     require __DIR__.'/../gradebook.php';
 
+    // Kehadiran mahasiswa dapat dikelola Administrator tanpa berpindah guard Dosen.
+    Route::get('/akademik/kehadiran', [App\\Http\\Controllers\\Private\\Dosen\\AkademikOperasionalController::class, 'webAdminKehadiran'])->name('akademik.kehadiran');
+    Route::post('/akademik/kehadiran', [App\\Http\\Controllers\\Private\\Dosen\\AkademikOperasionalController::class, 'webAdminSimpanKehadiran'])->name('akademik.kehadiran.store');
+
     // MASTER AUTHORITY
     require __DIR__.'/../master-core.php';
 });
