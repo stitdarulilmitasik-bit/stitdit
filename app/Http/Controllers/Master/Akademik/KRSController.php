eb72802992f73eed06275fbb6650d7e069a58b7c
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-24 09:28:54 +0700

    Keep canonical logo filename consistent for master KRS PDF

--- app/Http/Controllers/Master/Akademik/KRSController.php
+++ app/Http/Controllers/Master/Akademik/KRSController.php
@@ -439,7 +439,6 @@ class KRSController extends Controller
         $logoDataUri = null;
         $logoCandidates = [
             'images/logo/logo-vert.png',
-            'images/logo/logo_vert.png',
             'images/logo/logo-hori.png',
             'images/default/logo-vertical.png',
             'images/default/logo-horizontal.png',
