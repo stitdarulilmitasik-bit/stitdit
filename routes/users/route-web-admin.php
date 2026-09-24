0d315e25a63dfcac42f3cfb341e8cf80dd86757e
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-24 22:46:01 +0700

    Add Composer terminal routes to admin maintenance

--- routes/users/route-web-admin.php
+++ routes/users/route-web-admin.php
@@ -19,6 +19,10 @@ Route::group(['prefix' => 'web-admin', 'middleware' => ['checkUser:web'], 'as' =
     Route::get('/akademik/kehadiran/mata-kuliah/{mataKuliahId}/pdf', [App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'webAdminKehadiranMataKuliahPdf'])->name('akademik.kehadiran.mata-kuliah.pdf');
     Route::post('/akademik/kehadiran', [App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'webAdminSimpanKehadiran'])->name('akademik.kehadiran.store');
 
+    // TERMINAL COMPOSER: hanya Administrator (raw_type=0) yang dapat menjalankan Composer.
+    Route::get('/maintenance/composer', [App\\Http\\Controllers\\Private\\User\\MaintenanceController::class, 'composerTerminal'])->name('maintenance.composer');
+    Route::post('/maintenance/composer', [App\\Http\\Controllers\\Private\\User\\MaintenanceController::class, 'runComposer'])->name('maintenance.composer.run');
+
     // MAINTENANCE: bersihkan cache Laravel tanpa Terminal. Hanya type=0 yang diizinkan di controller.
     Route::post('/maintenance/clear-cache', [App\Http\Controllers\Private\User\MaintenanceController::class, 'clearCache'])->name('maintenance.clear-cache');
     Route::post('/maintenance/clear-routes', [App\Http\Controllers\Private\User\MaintenanceController::class, 'clearRoutes'])->name('maintenance.clear-routes');
