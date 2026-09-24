2b8298a9e3f694dfae44b99eb50519982902c0ae
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-24 09:27:50 +0700

    Support deployed logo filename variants for KRS PDF

--- app/Http/Controllers/Private/Mahasiswa/AkademikController.php
+++ app/Http/Controllers/Private/Mahasiswa/AkademikController.php
@@ -108,7 +108,9 @@ class AkademikController extends Controller
         $logoDataUri = null;
         foreach ([
             'images/logo/logo-vert.png',
+            'images/logo/logo_vert.png',
             'images/logo/logo-hori.png',
+            'images/logo/logo_hori.png',
             'images/logo/logo_hori.png',
             'images/default/logo-vertical.png',
             'images/default/logo-horizontal.png',
