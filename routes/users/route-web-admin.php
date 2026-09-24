e9fefe7ba8b1f86ecc3327dca34de349f319dc5e
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-24 07:43:38 +0700

    Add web admin cache clear route

--- routes/users/route-web-admin.php
+++ routes/users/route-web-admin.php
@@ -19,6 +19,9 @@ Route::group(['prefix' => 'web-admin', 'middleware' => ['checkUser:web'], 'as' =
     Route::get('/akademik/kehadiran/mata-kuliah/{mataKuliahId}/pdf', [App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'webAdminKehadiranMataKuliahPdf'])->name('akademik.kehadiran.mata-kuliah.pdf');
     Route::post('/akademik/kehadiran', [App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'webAdminSimpanKehadiran'])->name('akademik.kehadiran.store');
 
+    // MAINTENANCE: bersihkan cache Laravel tanpa Terminal. Hanya type=0 yang diizinkan di controller.
+    Route::post('/maintenance/clear-cache', [App\\Http\\Controllers\\Private\\User\\MaintenanceController::class, 'clearCache'])->name('maintenance.clear-cache');
+
     // MASTER AUTHORITY
     require __DIR__.'/../master-core.php';
 });
