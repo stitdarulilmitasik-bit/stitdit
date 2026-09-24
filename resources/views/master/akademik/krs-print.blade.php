793c16ce16311870996c543dcbd4866f3f58af10
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-24 15:12:36 +0700

    Render KRS logo from media URL

--- resources/views/master/akademik/krs-print.blade.php
+++ resources/views/master/akademik/krs-print.blade.php
@@ -280,7 +280,7 @@
         ?? $dosenWali?->number_nidn
         ?? '-';
 
-    $logo = $logoDataUri ?? null;
+    $logo = $logoUrl ?? null;
 
     $tahunAkademikRaw = (string) ($krs->tahunAkademik->name ?? '');
     if (preg_match('/(\d{4}\s*\/\s*\d{4})/', $tahunAkademikRaw, $matches)) {
@@ -294,8 +294,8 @@
     <table class="kop-table">
         <tr>
             <td class="kop-logo">
-                @if (!empty($logoDataUri))
-                    <img src="{{ $logoDataUri }}" alt="Logo STIT Darul Ilmi" width="68" height="68" style="display:block;margin:0 auto;">
+                @if (!empty($logoUrl))
+                    <img src="{{ $logoUrl }}" alt="Logo STIT Darul Ilmi" width="68" height="68" style="display:block;margin:0 auto;">
                 @endif
             </td>
             <td class="kop-text">
