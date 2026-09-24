cfa9395a70a231ccf314be89294a390ec5e1f8ba
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-24 22:46:08 +0700

    Fix Composer maintenance route namespace escaping

--- routes/users/route-web-admin.php
+++ routes/users/route-web-admin.php
@@ -20,8 +20,8 @@ Route::group(['prefix' => 'web-admin', 'middleware' => ['checkUser:web'], 'as' =
     Route::post('/akademik/kehadiran', [App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'webAdminSimpanKehadiran'])->name('akademik.kehadiran.store');
 
     // TERMINAL COMPOSER: hanya Administrator (raw_type=0) yang dapat menjalankan Composer.
-    Route::get('/maintenance/composer', [App\\Http\\Controllers\\Private\\User\\MaintenanceController::class, 'composerTerminal'])->name('maintenance.composer');
-    Route::post('/maintenance/composer', [App\\Http\\Controllers\\Private\\User\\MaintenanceController::class, 'runComposer'])->name('maintenance.composer.run');
+    Route::get('/maintenance/composer', [App\Http\Controllers\Private\User\MaintenanceController::class, 'composerTerminal'])->name('maintenance.composer');
+    Route::post('/maintenance/composer', [App\Http\Controllers\Private\User\MaintenanceController::class, 'runComposer'])->name('maintenance.composer.run');
 
     // MAINTENANCE: bersihkan cache Laravel tanpa Terminal. Hanya type=0 yang diizinkan di controller.
     Route::post('/maintenance/clear-cache', [App\Http\Controllers\Private\User\MaintenanceController::class, 'clearCache'])->name('maintenance.clear-cache');
