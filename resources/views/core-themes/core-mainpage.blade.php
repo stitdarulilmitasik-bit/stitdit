86751d44ce7d3cac4a7f2486ec05e0ab6b22fdda
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-24 13:07:14 +0700

    Fix homepage profile avatar icon

--- resources/views/core-themes/core-mainpage.blade.php
+++ resources/views/core-themes/core-mainpage.blade.php
@@ -433,7 +433,19 @@
                     </div>
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link d-flex lh-1 p-0 px-2" data-bs-toggle="dropdown" aria-label="Open user menu">
-                            <span class="avatar avatar-sm" style="background-image: url({{ $user == null ? stit_profile_image_url(null) : stit_profile_image_url($user->photo) }})" onerror="this.style.backgroundImage='url({{ stit_profile_image_url(null) }})'">
+                            <span class="avatar avatar-sm bg-primary-lt d-flex align-items-center justify-content-center" aria-hidden="true">
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                     width="22"
+                                     height="22"
+                                     viewBox="0 0 24 24"
+                                     fill="none"
+                                     stroke="currentColor"
+                                     stroke-width="2"
+                                     stroke-linecap="round"
+                                     stroke-linejoin="round">
+                                    <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
+                                    <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+                                </svg>
                             </span>
                             <div class="d-none d-xl-block ps-2">
                                 <div>{{ $user == null ? 'Guest' : $user->name }}</div>
