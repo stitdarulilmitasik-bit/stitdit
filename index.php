c63eb37855cb398e29eb62567e5dbdc266aa8387
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-25 23:42:45 +0700

    Fix ByetHost root entry point

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * ByetHost document-root entry point.
+ *
+ * The hosting document root is /htdocs while the Laravel
+ * application entry point lives in /htdocs/public/index.php.
+ */
+require __DIR__ . '/public/index.php';
