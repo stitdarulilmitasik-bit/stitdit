40048cc32874eb506211c2d565a868b78e2ffc1e
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-26 19:45:11 +0700

    Add Web Admin attendance PDF export route

--- routes/users/route-web-admin.php
+++ routes/users/route-web-admin.php
@@ -15,6 +15,7 @@ Route::group(['prefix' => 'web-admin', 'middleware' => ['checkUser:web'], 'as' =
 
     // Kehadiran mahasiswa dapat dikelola Administrator tanpa berpindah guard Dosen.
     Route::get('/akademik/kehadiran', [App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'webAdminKehadiran'])->name('akademik.kehadiran');
+    Route::get('/akademik/kehadiran/export-pdf', [App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'webAdminKehadiranExportPdf'])->name('akademik.kehadiran.export-pdf');
     Route::get('/akademik/kehadiran/{mahasiswaId}/pdf', [App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'webAdminKehadiranPdf'])->name('akademik.kehadiran.pdf');
     Route::get('/akademik/kehadiran/mata-kuliah/{mataKuliahId}/pdf', [App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'webAdminKehadiranMataKuliahPdf'])->name('akademik.kehadiran.mata-kuliah.pdf');
     Route::post('/akademik/kehadiran', [App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'webAdminSimpanKehadiran'])->name('akademik.kehadiran.store');
