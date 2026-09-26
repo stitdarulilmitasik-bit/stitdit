1e7acbd441b8a139ffe4f4de6432a54b4d21622c
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-26 20:23:25 +0700

    Add migration maintenance route

--- routes/users/route-web-admin.php
+++ routes/users/route-web-admin.php
@@ -30,6 +30,7 @@ Route::group(['prefix' => 'web-admin', 'middleware' => ['checkUser:web'], 'as' =
     Route::post('/maintenance/composer', [App\Http\Controllers\Private\User\MaintenanceController::class, 'runComposer'])->name('maintenance.composer.run');
 
     // MAINTENANCE: bersihkan cache Laravel tanpa Terminal. Hanya type=0 yang diizinkan di controller.
+    Route::post('/maintenance/migrate', [App\\Http\\Controllers\\Private\\User\\MaintenanceController::class, 'migrate'])->name('maintenance.migrate');
     Route::post('/maintenance/clear-cache', [App\Http\Controllers\Private\User\MaintenanceController::class, 'clearCache'])->name('maintenance.clear-cache');
     Route::post('/maintenance/storage-link', [App\Http\Controllers\Private\User\MaintenanceController::class, 'storageLink'])->name('maintenance.storage-link');
     Route::post('/maintenance/clear-routes', [App\Http\Controllers\Private\User\MaintenanceController::class, 'clearRoutes'])->name('maintenance.clear-routes');
