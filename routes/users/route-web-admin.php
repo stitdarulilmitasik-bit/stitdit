6cb6c6af6baa7424909e191ea73efe058eae7212
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-26 20:25:54 +0700

    Fix namespace syntax in web admin routes

--- routes/users/route-web-admin.php
+++ routes/users/route-web-admin.php
@@ -21,16 +21,16 @@ Route::group(['prefix' => 'web-admin', 'middleware' => ['checkUser:web'], 'as' =
     Route::post('/akademik/kehadiran', [App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'webAdminSimpanKehadiran'])->name('akademik.kehadiran.store');
 
     // LAYANAN MAHASISWA => LEGALISIR
-    Route::get('/layanan/legalisir', [App\\Http\\Controllers\\Master\\Layanan\\LegalisirController::class, 'index'])->name('layanan.legalisir');
-    Route::get('/layanan/legalisir/{id}', [App\\Http\\Controllers\\Master\\Layanan\\LegalisirController::class, 'show'])->name('layanan.legalisir.detail');
-    Route::patch('/layanan/legalisir/{id}', [App\\Http\\Controllers\\Master\\Layanan\\LegalisirController::class, 'update'])->name('layanan.legalisir.update');
+    Route::get('/layanan/legalisir', [App\Http\Controllers\Master\Layanan\LegalisirController::class, 'index'])->name('layanan.legalisir');
+    Route::get('/layanan/legalisir/{id}', [App\Http\Controllers\Master\Layanan\LegalisirController::class, 'show'])->name('layanan.legalisir.detail');
+    Route::patch('/layanan/legalisir/{id}', [App\Http\Controllers\Master\Layanan\LegalisirController::class, 'update'])->name('layanan.legalisir.update');
 
     // TERMINAL COMPOSER: hanya Administrator (raw_type=0) yang dapat menjalankan Composer.
     Route::get('/maintenance/composer', [App\Http\Controllers\Private\User\MaintenanceController::class, 'composerTerminal'])->name('maintenance.composer');
     Route::post('/maintenance/composer', [App\Http\Controllers\Private\User\MaintenanceController::class, 'runComposer'])->name('maintenance.composer.run');
 
     // MAINTENANCE: bersihkan cache Laravel tanpa Terminal. Hanya type=0 yang diizinkan di controller.
-    Route::post('/maintenance/migrate', [App\\Http\\Controllers\\Private\\User\\MaintenanceController::class, 'migrate'])->name('maintenance.migrate');
+    Route::post('/maintenance/migrate', [App\Http\Controllers\Private\User\MaintenanceController::class, 'migrate'])->name('maintenance.migrate');
     Route::post('/maintenance/clear-cache', [App\Http\Controllers\Private\User\MaintenanceController::class, 'clearCache'])->name('maintenance.clear-cache');
     Route::post('/maintenance/storage-link', [App\Http\Controllers\Private\User\MaintenanceController::class, 'storageLink'])->name('maintenance.storage-link');
     Route::post('/maintenance/clear-routes', [App\Http\Controllers\Private\User\MaintenanceController::class, 'clearRoutes'])->name('maintenance.clear-routes');
