d353585f362c5d4028f31ffe6d78a09f89a2d6d7
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-26 16:44:17 +0700

    Fix guest user variable on public pages

--- resources/views/core-themes/core-mainpage.blade.php
+++ resources/views/core-themes/core-mainpage.blade.php
@@ -459,12 +459,12 @@
                                 </span>
                             </span>
                             <div class="d-none d-xl-block ps-2">
-                                <div>{{ $user == null ? 'Guest' : $user->name }}</div>
-                                <div class="mt-1 small text-secondary">{{ $user == null ? 'Guest' : $user->type }}</div>
+                                <div>{{ ($user ?? null) == null ? 'Guest' : $user->name }}</div>
+                                <div class="mt-1 small text-secondary">{{ ($user ?? null) == null ? 'Guest' : $user->type }}</div>
                             </div>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                            @if ($user)
+                            @if ($user ?? null)
                             <a href="{{ route($spref.'profile-render') }}" class="dropdown-item">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-inline me-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                     <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
