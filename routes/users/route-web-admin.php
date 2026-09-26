b235d65249a20ce62604ca10a3e4a8d5b85559c6
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-26 20:22:41 +0700

    Add web admin legalisir routes

--- routes/users/route-web-admin.php
+++ routes/users/route-web-admin.php
@@ -20,6 +20,11 @@ Route::group(['prefix' => 'web-admin', 'middleware' => ['checkUser:web'], 'as' =
     Route::get('/akademik/kehadiran/mata-kuliah/{mataKuliahId}/pdf', [App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'webAdminKehadiranMataKuliahPdf'])->name('akademik.kehadiran.mata-kuliah.pdf');
     Route::post('/akademik/kehadiran', [App\Http\Controllers\Private\Dosen\AkademikOperasionalController::class, 'webAdminSimpanKehadiran'])->name('akademik.kehadiran.store');
 
+    // LAYANAN MAHASISWA => LEGALISIR
+    Route::get('/layanan/legalisir', [App\\Http\\Controllers\\Master\\Layanan\\LegalisirController::class, 'index'])->name('layanan.legalisir');
+    Route::get('/layanan/legalisir/{id}', [App\\Http\\Controllers\\Master\\Layanan\\LegalisirController::class, 'show'])->name('layanan.legalisir.detail');
+    Route::patch('/layanan/legalisir/{id}', [App\\Http\\Controllers\\Master\\Layanan\\LegalisirController::class, 'update'])->name('layanan.legalisir.update');
+
     // TERMINAL COMPOSER: hanya Administrator (raw_type=0) yang dapat menjalankan Composer.
     Route::get('/maintenance/composer', [App\Http\Controllers\Private\User\MaintenanceController::class, 'composerTerminal'])->name('maintenance.composer');
     Route::post('/maintenance/composer', [App\Http\Controllers\Private\User\MaintenanceController::class, 'runComposer'])->name('maintenance.composer.run');
