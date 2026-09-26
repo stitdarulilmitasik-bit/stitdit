188f6a47c96a9019fa21f57557fc5d43617fc5bc
stitdarulilmitasik-bit <stit.darul.ilmi.tasik@gmail.com>
2026-09-26 20:02:19 +0700

    Perbaiki footer PDF agar tidak ganda

--- resources/views/private/dosen/kehadiran-mata-kuliah-pdf.blade.php
+++ resources/views/private/dosen/kehadiran-mata-kuliah-pdf.blade.php
@@ -116,8 +116,8 @@
         color: #555; 
     }
 
-.pdf-system-footer{position:fixed;left:0;right:0;bottom:-7mm;border-top:1px solid #777;padding-top:2px;text-align:center;font-size:6.5pt;color:#555}.pdf-system-footer .page:after{content:"Halaman " counter(page) " dari " counter(pages);}
-.footer .page:after{content:"Halaman " counter(page) " dari " counter(pages)}
+.pdf-system-footer .page:after{content:"Halaman " counter(page) " dari " counter(page);}
+.footer .page:after{content:"Halaman " counter(page) " dari " counter(page)}
 </style>
 </head>
 <body>
